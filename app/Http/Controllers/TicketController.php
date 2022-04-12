d41fba82f70b7a280e913f2fec7cdde0a36fd02c
Nilson Junior <nilsonjrsilva97@gmail.com>
2022-04-12 09:41:47 -0300

    fix in tickets

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Ticket;
+use Auth;
 
 class TicketController extends Controller
 {
     public function index()
     {
-        return Ticket::all();
+        return Auth::user()->tickets()->get();
     }
 
     public function show($id)
@@ -21,28 +22,30 @@ class TicketController extends Controller
     public function create(Request $request)
     {
         $validatedData = $request->validate([
-                    'bar_code' => 'required|string|max:255',
+            'bar_code' => 'required|string|max:255',
             'file' => 'required|string|max:255',
             'due_date' => 'required|date',
-            'user_id' => 'required|integer',
         ]);
+
+        $validatedData['user_id'] = Auth::id();
+
         return Ticket::create($validatedData);
     }
     
     public function update(Request $request, $id)
     {
         $validatedData = $request->validate([
-                    'bar_code' => 'string|max:255',
+            'bar_code' => 'string|max:255',
             'file' => 'string|max:255',
             'due_date' => 'date',
-            'user_id' => 'integer',
         ]);
+
         $ticket = Ticket::findOrFail($id);
         $ticket->fill($validatedData);
         $ticket->save();
         return $ticket;
     }
-    
+
     public function destroy($id)
     {
         $ticket = Ticket::findOrFail($id);
